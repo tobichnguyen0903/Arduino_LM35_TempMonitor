da595ce083070775dfe2a92050cbce68bee62acf
To Bich Nguyen <tobichnguyen936@gmail.com>
2026-04-12 02:10:00 +0700

    Initial commit: cau truc du an va firmware doc LM35 kenh A0

---
+++ firmware/LM35_TempReader/LM_35TempReader/LM_35TempReader.ino
@@ -0,0 +1,19 @@
+/*
+ * Du an: Doc nhiet do LM35 va gui qua Serial
+ * Mon: Mang Cam Bien - Tuan 03
+ */
+
+int adcValue;
+float nhietDo;
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+}
+
+void loop() {
+  adcValue = analogRead(A0);
+  nhietDo = (adcValue * 500.0) / 1023.0;
+  Serial.println(nhietDo, 1);
+  delay(1000);
+}
