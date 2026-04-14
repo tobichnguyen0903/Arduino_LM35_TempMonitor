83d22220cab924892e36ff56c032faf20be91990
To Bich Nguyen <tobichnguyen936@gmail.com>
2026-04-14 18:29:03 +0700

    feat: them kenh A2

--- firmware/LM35_TempReader/LM_35TempReader/LM_35TempReader.ino
+++ firmware/LM35_TempReader/LM_35TempReader/LM_35TempReader.ino
@@ -3,9 +3,10 @@
  * Mon: Mang Cam Bien - Tuan 03
  */
 
-int adcValue[2];
-float nhietDo[2];
-char chuoi [30];
+// SỬA: Tăng kích thước mảng lên 3 và đổi tên thành adcValues cho khớp với loop
+int adcValues[3];  
+float nhietDo[3];
+char chuoi[40]; // SỬA: Tăng kích thước chuỗi lên 40 để chứa thêm dữ liệu
 
 void setup() {
   Serial.begin(9600);
@@ -13,11 +14,21 @@ void setup() {
 }
 
 void loop() {
+  // Đọc kênh A0
   adcValues[0] = analogRead(A0);
   nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
+  // Đọc kênh A1
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+
+  // THÊM MỚI: Đọc kênh A2
+  adcValues[2] = analogRead(A2);
+  nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
+
+  // SỬA: Thêm %d thứ 3 và đưa nhietDo[2] vào để in ra
+  sprintf(chuoi, "%d,%d,%d\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
+  
   Serial.print(chuoi);
   delay(100);
 }
